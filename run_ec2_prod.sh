30303c87783e2287fb8d769bfb90c8f4b0d7e6ba
James Michael DuPont <James.DuPont@nrg.com>
2021-03-05 18:18:34 +0000

    all

---
+++ run_ec2_prod.sh
@@ -0,0 +1,4 @@
+docker  run \
+	--mount type=bind,source="${HOME}/.aws/",target=/root/.aws \
+	--mount type=bind,source="${HOME}/pyinsights/queries/",target=/opt/pyinsights/queries/ \
+	-it "pyinsights" pyinsights -c /opt/pyinsights/queries/all.yml --profile prod -r us-east-1 > all_prod.json
